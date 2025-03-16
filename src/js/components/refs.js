c6d15715e268ee2eeb25108bffd52bf04451a93c
Eugen Alekseev <devilsbrother1@gmail.com>
2025-03-17 00:00:30 +0200

    add header + modal menu

---
+++ src/js/components/refs.js
@@ -0,0 +1,6 @@
+let choose = selector => document.querySelector(selector);
+
+export default {
+	overlay: choose('#overlay'),
+	modal: choose('#modal'),
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,64 @@
 import './main.scss';
-
+import refs from './js/components/refs';
 import './js/blocks/hero';
 
-console.log('Js here');
+window.addEventListener('resize', () => {
+	if (window.innerWidth > 1024) {
+		closeModal();
+	}
+});
 
+document.addEventListener('click', function (event) {
+	const button = event.target.closest('button');
+	if (button) {
+		const dataset = button.dataset;
+		const key = Object.keys(dataset)[0];
+		const value = dataset[key];
+		if (!button.classList.contains('open')) {
+			button.classList.add('open');
+			showContent(key, value);
+		} else {
+			button.classList.remove('open');
+			hideContent(key, value);
+		}
+	}
+});
 
+refs.overlay.addEventListener('click', event => {
+	refs.overlay.classList.add('visually-hidden');
+	refs.modal.classList.add('visually-hidden');
+	Array.from(refs.modal.children).forEach(li =>
+		li.classList.add('visually-hidden')
+	);
+	closeButtons();
+});
+
+function showContent(key, value) {
+	refs.overlay.classList.remove('visually-hidden');
+	refs[key].classList.remove('visually-hidden');
+	refs[key]
+		.querySelector(`.${key}__${value}`)
+		.classList.remove('visually-hidden');
+}
+function hideContent(key, value) {
+	refs.overlay.classList.add('visually-hidden');
+	refs[key].classList.add('visually-hidden');
+	refs[key].querySelector(`.${key}__${value}`).classList.add('visually-hidden');
+}
+
+function closeModal() {
+	if (!refs.modal.children[0].classList.contains('visually-hidden')) {
+		refs.modal.children[0].classList.add('visually-hidden');
+		refs.modal.classList.add('visually-hidden');
+		refs.overlay.classList.add('visually-hidden');
+		closeButtons();
+	}
+}
+
+function closeButtons() {
+	if (document.querySelectorAll('.open')) {
+		document
+			.querySelectorAll('.open')
+			.forEach(btn => btn.classList.remove('open'));
+	}
+}
